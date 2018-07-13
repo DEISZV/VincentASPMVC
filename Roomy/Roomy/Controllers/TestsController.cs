fa178e4f9071d97e1f66c1ecbc197e3b3d4af700
Vincent <vincentdeisz@hotmail.fr>
2018-07-13 16:44:08 +0200

    MVC

---
+++ Roomy/Roomy/Controllers/TestsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roomy.Controllers
+{
+    public class TestsController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
